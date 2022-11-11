c4ec46f2f7f7de8ad900ba93db23be0fae5ac046
Todd L Smith <todd@availlang.org>
2022-11-10 18:36:18 -0600

    Updated `avail-artifact` dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ plugins {
 //}
 
 group = "org.availlang"
-version = "2.0.0.alpha13"
+version = "2.0.0.alpha14"
 
 
 /**
@@ -118,7 +118,7 @@ kotlin {
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-	api("org.availlang:avail-artifact:2.0.0.alpha11")
+	api("org.availlang:avail-artifact:2.0.0.alpha12")
 }
 
 tasks {
